baf4ce0178472b175a53d8269714e886c3149c31
youngkwon315 <39914288+youngkwon315@users.noreply.github.com>
2020-02-24 23:38:45 +0900

    200224 Changes - added color change feature

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+canvas.width = 700;
+canvas.height = 700;
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
@@ -17,22 +21,27 @@ function startPainting() {
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  if(!painting){
+  if (!painting) {
     ctx.beginPath();
-    ctx.moveTo(x,y);
+    ctx.moveTo(x, y);
   } else {
-    ctx.lineTo(x,y);
+    ctx.lineTo(x, y);
     ctx.stroke();
   }
 }
 
-function onMouseDown(event) {
-  painting = true;
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
-  canvas.addEventListener("mouseleave", stopPainting;
+  canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color =>
+  color.addEventListener("click", handleColorClick)
+);
